287e12cac1ea54bdb4430d2dd6a1ec692ab0321b
Lev Pikulev <pikulev.l.v@gmail.com>
2020-08-28 11:33:16 +0300

    Домашняя работа закончена

--- HW_2/classes.py
+++ HW_2/classes.py
@@ -1,7 +1,11 @@
 import re
 import random
 from abc import ABCMeta, abstractmethod
-from exceptions import NegativeValError
+from exceptions import NegativeValError, TypeValError
+from dataclasses import dataclass, field
+from typing import Optional, Union, List, Tuple, Dict
+from pprint import pprint
+
 
 class BaseVehicle(metaclass=ABCMeta):
     @abstractmethod
@@ -16,9 +20,45 @@ class BaseVehicle(metaclass=ABCMeta):
     @abstractmethod
     def make_sound(self) -> None:
         raise NotImplementedError
-    # @abstractmethod
-    # def make_sound2(self) -> None:
-    #     raise NotImplementedError
+
+class BaseEngine(metaclass=ABCMeta):
+
+    @abstractmethod
+    def crash(self):
+        ...
+@dataclass(init=True, repr=True, eq=True, order=False, unsafe_hash=False, frozen=False)
+class Engine(BaseEngine):
+    e_model: str
+    e_volume: float
+    e_piston: int
+    e_fuel_consumption: float
+    e_brake_down: bool
+    e_features: Dict[str, str] = field(default_factory=dict)
+
+    def __post_init__(self):
+        if not isinstance(self.e_model, str):
+            raise TypeValError(self.e_model)
+        if not isinstance(self.e_volume, float):
+            raise TypeValError(self.e_volume)
+        if not isinstance(self.e_piston, int):
+            raise TypeValError(self.e_piston)
+        if not isinstance(self.e_fuel_consumption, float):
+            raise TypeValError(self.e_fuel_consumption)
+        if not isinstance(self.e_brake_down, bool):
+            raise TypeValError(self.e_brake_down)
+        if not isinstance(self.e_features, dict):
+            raise TypeValError(self.e_features)
+        for ef in self.e_features:
+            if not isinstance(ef, str):
+                raise TypeValError(ef)
+
+    def crash(self):
+        print("Tuk-tuk-tuk!!! Crash the engine!!!")
+        self.e_brake_down = True
+
+    def get_engine_features(self):
+        for k, v in self.e_features.items():
+            print(f"{k}: {v}")
 
 
 class MechanicVehicle(BaseVehicle):
@@ -29,16 +69,16 @@ class MechanicVehicle(BaseVehicle):
     FUEL_TANK = 45
 
     def __init__(self, producer, model, mileage,
-                 release_year, fuel_consumption, fuel=0):
+                 release_year, engine, fuel=0):
         self.__producer = producer
         self.__model = model
         self.__mileage = mileage
         self.__release_year = release_year
-        self.fuel_consumption = fuel_consumption
+        self.engine = engine
         self.__fuel = fuel
-        
+
         numeric_val_list = [self.TONNAGE, self.WEIGHT, self.FUEL_TANK, self.__mileage,
-                            self.__release_year, self.fuel_consumption, self.__fuel]
+                            self.__release_year, self.__fuel]
         if sum(map(lambda x: isinstance(x, (int, float)), numeric_val_list)) != len(numeric_val_list):
             print("Please, call get_features() and check data type of numeric variables.")
 
@@ -46,7 +86,7 @@ class MechanicVehicle(BaseVehicle):
         return f"{self.__class__.__name__} {self.__producer} {self.__model} {self.__release_year}"
     def __repr__(self):
         return f"{self.__class__.__name__} {self.__producer} {self.__model} {self.__release_year}"
-
+    @property
     def get_mileage(self):
         return self.__mileage
 
@@ -54,10 +94,13 @@ class MechanicVehicle(BaseVehicle):
         return self.__fuel
 
     def move(self, distance):
+        if self.engine.e_brake_down is True:
+            print(f"{self} can not move, because it's engine broke down.")
+            return
         if distance < 0:
             raise NegativeValError(distance)
         # расчет кол-ва топлива
-        need_fuel = distance * self.fuel_consumption
+        need_fuel = distance/100 * self.engine.e_fuel_consumption
         # проверка достаточности топлива
         if self.__fuel >= need_fuel:
             # расходование топлива
@@ -78,7 +121,7 @@ class MechanicVehicle(BaseVehicle):
         if total_fuel > self.FUEL_TANK:
             lost_fuel = total_fuel - self.FUEL_TANK
             self.__fuel = self.FUEL_TANK
-            print(f"{self.__producer} {self.__model} was filled up with {__fuel_units} units of fuel."
+            print(f"{self.__producer} {self.__model} was filled up with {fuel_units} units of fuel."
             f" Unfortunately, {lost_fuel} units of fuel were lost. Fuel rank: {self.__fuel}.")
         else:
             self.__fuel += fuel_units
@@ -89,7 +132,6 @@ class MechanicVehicle(BaseVehicle):
         pattern = '^[A-Z]+'
         basic_attr_list = [re.search(pattern, i).string for i in dir(self) if (re.search(pattern, i) != None)]
         add_attr_list = [i for i in self.__dict__.keys() if i not in basic_attr_list]
-        print(self.__class__.__name__)
         print(f"{self} has basic features:")
         for a in basic_attr_list:
             print('\t'+a)
@@ -116,18 +158,19 @@ class Car(MechanicVehicle):
         self.SPORT_MODE = not self.SPORT_MODE
 
         if self.SPORT_MODE == True:
-            self.fuel_consumption *= sport_mode_coef
-            print(f'Turn on sport mode! Fuel consumption: {self.fuel_consumption} fuel units per km.')
+            self.engine.e_fuel_consumption *= sport_mode_coef
+            print(f'Turn on sport mode! Fuel consumption: {self.engine.e_fuel_consumption} fuel units per km.')
         elif self.SPORT_MODE == False:
-            self.fuel_consumption /= sport_mode_coef
-            print(f'Turn off sport mode! Fuel consumption: {self.fuel_consumption} fuel units per km.')
+            self.engine.e_fuel_consumption /= sport_mode_coef
+            print(f'Turn off sport mode! Fuel consumption: {self.engine.e_fuel_consumption} fuel units per km.')
 
 class Jet(MechanicVehicle):
     WHEELS = 3
     WINGS = 2
+    SOUND = 'fffff'
+    FUEL_TANK = 500
 
     def permission_to_fly(self):
-        # print('Pilot: requesting permission to take off.')
         permission = random.choices([True, False], weights=[60, 40])[0]
         if permission:
             print('Dispatcher: Permission to take off.')
@@ -191,17 +234,20 @@ class AmphibiaCar(Car, Ship):
 
 
 if __name__ == '__main__':
-    some_vhcl = MechanicVehicle('KIA', 'Rio', 22_000, 2019, 8/100, 0)
+    some_engine = Engine(e_model='Some Model', e_volume=2.0, e_piston=8, e_fuel_consumption=8.0,
+                         e_brake_down=False, e_features={'f_1':'value...'})
+    some_vhcl = MechanicVehicle('KIA', 'Rio', 22_000, 2019, some_engine, 0)
     print(some_vhcl)
     some_vhcl.move(15)
     some_vhcl.fill_up(1.2)
     some_vhcl.move(15)
     some_vhcl.make_sound()
-    # some_vhcl.set_sound('beep')
     some_vhcl.make_sound()
     some_vhcl.get_features()
     print('*'*30)
-    ship = Ship('V5', 'Mercury', 1_000_000, 1995, 80, 40)
+    ship_engine = Engine(e_model='Ship Model', e_volume=20.0, e_piston=16, e_fuel_consumption=80.,
+                         e_brake_down=False, e_features={'f_1':'value...'})
+    ship = Ship('V5', 'Mercury', 1_000_000, 1995, ship_engine, 40)
     ship.TONNAGE = 1_000_000
     ship.ENVIRONMENT = 'marine'
     ship.WEIGHT = 2_000_000
@@ -212,7 +258,9 @@ if __name__ == '__main__':
     ship.make_sound()
     ship.get_features()
     print('*' * 30)
-    car = Car('Kia', 'Sportage', 5, 2020, 8.5, 5)
+    car_engine = Engine(e_model='Car Model', e_volume=1.6, e_piston=6, e_fuel_consumption=8.2,
+                        e_brake_down=False, e_features={'f_1':'value...'})
+    car = Car('Kia', 'Sportage', 5, 2020, car_engine, 5)
     car.TONNAGE = 400
     car.WEIGHT = 1300
     car.SOUND = 'beep'
@@ -225,7 +273,9 @@ if __name__ == '__main__':
     car.sport_mode()
     car.sport_mode()
     print('*' * 30)
-    jet = Jet('Sukhoi', 'SuperJet', 700_000, 2018, 100, 50000)
+    jet_engine = Engine(e_model='Aero Model', e_volume=180., e_piston=32, e_fuel_consumption=100., e_brake_down=False,
+                        e_features={'f_1':'value...'})
+    jet = Jet('Sukhoi', 'SuperJet', 700_000, 2018, jet_engine, 50000)
     jet.TONNAGE = 2000
     jet.ENVIRONMENT = 'air'
     jet.WEIGHT = 5000
@@ -241,18 +291,15 @@ if __name__ == '__main__':
     jet.move(100)
     print(jet)
     print('*' * 30)
-    amphibia = AmphibiaCar('Amphicar ', '770', 150_000, '2001', 10, 2)
+    amphibia_engine = Engine(e_model='WaterTerra Model', e_volume=1.2, e_piston=4, e_fuel_consumption=10.,
+                             e_brake_down=False, e_features={'f_1':'value...'})
+    amphibia = AmphibiaCar('Amphicar ', '770', 150_000, 2001, amphibia_engine, 2)
     print(amphibia)
-    # amphibia.TONNAGE = 100
-    # amphibia.ENVIRONMENT = 'terrestrial/marine'
-    # amphibia.WEIGHT = 670
-    # amphibia.SOUND = 'bul beep bul'
-    # amphibia.FUEL_TANK = 30
     amphibia.alarm()
     amphibia.set_basic_features()
-    # amphibia.fill_up(-5)
     amphibia.fill_up(fuel_units=5)
     amphibia.get_features()
+    print('Mileage:', amphibia.get_mileage, 'km')
 
 
 
--- HW_2/main.py
+++ HW_2/main.py
@@ -1,12 +1,98 @@
-from classes import Car, Jet, Ship
+# Домашнее задание №2. Классы.
+# Цель: в этом ДЗ написать базовый класс и сделать главный модуль,
+# при запуске которого выполняется демонстрация работы всех классов.
 
-ship = Ship('V5', 'Mercury', 1_000_000, 1995, 80, 40)
-ship.TONNAGE = 1_000_000
-ship.ENVIRONMENT = 'marine'
-ship.WEIGHT = 2_000_000
-ship.SOUND = 'tooo'
-ship.FUEL_TANK = 1000
-ship.alarm()
+# 1. Написать базовый класс для средства передвижения, определить у него общие свойства.Например, вес, грузоподъемность.
+# 2. Определить общие методы, например “издать звук”.
+# 3. Сделать несколько наследников класса. Например: корабль, автомобиль, самолёт.
+# 4. Перегрузить у них методы, чтобы соответствовать классу.
+# 5. Также добавить свойства, относящиеся к классу: водоизмещение, количество колёс, предельная высота и т.д.
+# 6. Базовые классы должны быть реализованы асбстрактными классами при помощи модуля abc.
+# 7. Создать более конкретные классы. Например: “легковой автомобиль”, “парусная лодка” и тд.
+# 8. Перегрузить общие свойства, а также добавить дополнительные свойства и методы.
+# 9. Разнести все классы и исключения в отдельные модули.
+# 10. Добавить методы, которые принимают параметры.
+# 11. Добавить в инициализатор и другие методы классов выкидывание исключения при передаче неподходящих аргументов,
+# или не выполнении других внутренних условий (например, если бак пустой,
+# то при попытке “завести” ТС будет выкинуто исключение, что недостаточно топлива).
+# 12. Можно создать вспомогательные объекты при помощи датаклассов. Например, объект “двигатель”,
+# у которого есть несколько свойств: литраж, количество поршней, максимальное количество оборотов.
+# 13. Сделать главный модуль, при запуске которого выполняется демонстрация работы всех классов
+# (красивые принты с использованием print приветствуются).
+
+from classes import Car, Jet, Ship, AmphibiaCar, Engine
+from exceptions import NegativeValError
+
+car_engine = Engine(e_model='Mitsubishi У1', e_volume=2.0, e_piston=8,
+                     e_fuel_consumption=8.0, e_brake_down=False, e_features={
+        'Преимущества':'Довольно экономичный',
+        'Недостатки':'Ненадежный, скачут обороты',
+        })
+print("Create a car.")
+car = Car(producer='Hyundai', model='i40', mileage=22_000, release_year=2019, engine=car_engine, fuel=0)
+print(car)
+car.make_sound()
+print('Check mileage:', car.get_mileage, 'km.')
+print("Let's go 100 km!")
+car.move(100)
+print('Check fuel:', car.get_fuel(), 'l.')
+print("We have to fill the car. Bought 10 l of fuel.")
+car.fill_up(10)
+print("Let's go 100 km!")
+car.move(100)
+print(f"Good! Check fuel consumption: {car.engine.e_fuel_consumption} l per 100 km.")
+car.sport_mode()
+print(f"Check fuel consumption: {car.engine.e_fuel_consumption} l per 100 km.")
+car.engine.crash()
+print("Trying to go...")
+car.move(1)
+print("Write about the engine...")
+car.engine.get_engine_features()
+print('*'*60)
+print("We need to change the transport.")
+ship_engine = Engine(e_model='Model SE', e_volume=20.0, e_piston=16, e_fuel_consumption=80.,
+                         e_brake_down=False, e_features={'Преимущества':'Тихий'})
+print("Create a ship.")
+ship = Ship('V5', 'Mercury', 1_000_000, 1995, ship_engine, 40)
 print(ship)
 ship.make_sound()
+print("Check technical parameters.")
 ship.get_features()
+print('Check fuel:', ship.get_fuel(), 'l.')
+print("We have to fill the ship. Try to fill negative number of fuel.")
+try:
+    ship.fill_up(-10)
+except NegativeValError as e:
+    print(e)
+print('Try to go negative number of km.')
+try:
+    ship.move(-100)
+except NegativeValError as e:
+    print(e)
+ship.alarm()
+print('*'*60)
+print("We need to change the transport.")
+jet_engine = Engine(e_model='Aero Model', e_volume=180., e_piston=32, e_fuel_consumption=100., e_brake_down=False,
+                    e_features={'Преимущества': 'Не определены'})
+print("Create a jet.")
+jet = Jet('Sukhoi', 'SuperJet', 700_000, 2018, jet_engine, 400)
+print(jet)
+jet.make_sound()
+print('Check fuel:', jet.get_fuel(), 'l.')
+print('Fuel tank volume:', jet.FUEL_TANK)
+print("We have to fill the jet. Bought 300 l of fuel.")
+jet.fill_up(300)
+while jet.get_fuel() > 200:
+    print("Pilot: need permission to fly.")
+    jet.move(300)
+print('*'*60)
+print("We need to change the transport.")
+amphibia_engine = Engine(e_model='WaterTerra Model', e_volume=1.2, e_piston=4, e_fuel_consumption=10.,
+                         e_brake_down=False, e_features={'Преимущества': 'Не определены'})
+print("Create an amphibia.")
+amphibia = AmphibiaCar('Amphicar ', '770', 150_000, 2001, amphibia_engine, 2)
+print(amphibia)
+amphibia.make_sound()
+print("Strange sound. Set features for the amphibia.")
+amphibia.set_basic_features()
+amphibia.make_sound()
