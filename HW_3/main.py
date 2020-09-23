0f7f2652c1a0d7245a13a06920044836b5673643
Lev Pikulev <pikulev.l.v@gmail.com>
2020-09-23 23:08:10 +0300

    завершил ДЗ 3

--- HW_3/main.py
+++ HW_3/main.py
@@ -14,47 +14,81 @@
 запись в базу данных выполняется в асинхронном виде - 1 балл
 соединение с базой данных закрывается при завершении работы - 1 балл
 
-Гугл
-AIzaSyA21_lungeZwngEwtwAa7O4agwVUM30RLg
-
-Мос ру
-Смехов
-Вениамин
-https://apidata.mos.ru/Docs
-ierwnrsl16s0@mail.ru
-35776b13f50be5e64783795d20f359a3
-
-https://apidata.mos.ru/v1/datasets?$inlinecount=allpages$foreign=true?&api_key=35776b13f50be5e64783795d20f359a3
-https://apidata.mos.ru/v1/datasets?$inlinecount=allpages$foreign=false?&api_key=35776b13f50be5e64783795d20f359a3
-https://apidata.mos.ru/v1/datasets/654/icon/s?&api_key=35776b13f50be5e64783795d20f359a3
-https://apidata.mos.ru/v1/datasets/654/icon/s?&api_key=35776b13f50be5e64783795d20f359a3
-
+Работаем с apidata.mos.ru
 """
 
 import asyncio
 from dataclasses import dataclass
+from datetime import datetime
 from aiohttp import ClientSession
 from loguru import logger
+import psycopg2
+
+from sqlalchemy import create_engine, Table, MetaData
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey, Date
+from sqlalchemy.ext.declarative import declarative_base # model's basis
+from sqlalchemy.orm import sessionmaker, scoped_session, relationship
+
+#import api key
 from const import secret_key
 
-# import psycopg2
-# from const import PASSWORD
+#make engine and Base
+engine = create_engine("sqlite:///mosru_data.db")
+Base = declarative_base(bind=engine)
+# make a session
+session_factory = sessionmaker(bind=engine)
+Session = scoped_session(session_factory)
+
+
+class Rubric(Base):
+    """Rubric"""
+    __tablename__ = 'rubrics_1'
+    id = Column(Integer, primary_key=True, unique=True)
+    text = Column(String(64), nullable=False, unique=True, default='UNIVERSAL_RUBRIC')
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(id={self.id}, username={self.text!r})"
+
+    def __repr__(self):
+        return str(self)
+
+class MosruDataset(Base):
+    """Dataset class"""
+    __tablename__ = 'datasets_1'
+    id = Column(Integer, primary_key=True)
+    id_from_mosru = Column(Integer)
+    caption = Column(String)
+    categoryid = Column(Integer)
+    publishdate = Column(String)
+    keywords = Column(String)
+    containsgeodata = Column(Boolean)
+    isforeign = Column(Boolean)
+    isarchive = Column(Boolean)
+    isnew = Column(Boolean)
+    lastupdatedate = Column(String)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    rubric = Column(Integer, ForeignKey("rubrics_1.id"), nullable=True)
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(id={self.id}, username={self.caption!r})" # representation !r
+    def __repr__(self):
+        return str(self)
 
 @dataclass
 class ApiService:
+    """Service class"""
     name: str
     url: str
 
+# make two different addresses
 address_1 = 'https://apidata.mos.ru/v1/datasets?$inlinecount=allpages$foreign=true?&api_key=' +\
     secret_key
-
 address_2 = 'https://apidata.mos.ru/v1/datasets?$skip=200&$top=200&$inlinecount=allpages' \
             '$orderby=caption?&api_key=' +\
             secret_key
-# ip_fields = ['Id', 'Caption', 'PublishDate', 'Keywords']
 
 SERVICES = [
-    ApiService("mosru_1",address_1), # название, ссылка
+    ApiService("mosru_1",address_1), # name, link
     ApiService("mosru_2",address_2),
     ApiService("mosru_1",address_1),
 ]
@@ -65,32 +99,29 @@ async def fetch(session: ClientSession, url: str) -> dict:
         return await response.json()
 
 async def fetch_data(apiservice: ApiService) -> list:
-    """Async function """
     async with ClientSession() as session:
             result = await fetch(session, apiservice.url)
     logger.info("Got result for {}", apiservice.name)
-    return result#['Items']
+    return result
 
-async def get_my_ip():
+async def get_data():
     coros = [fetch_data(s) for s in SERVICES]
-    # res = await fetch_data(SERVICES[0])
-
     done, pending = await asyncio.wait(
         coros,
-        # timeout=15, # если не выполнится за _ сек, бросай
+        timeout=15, # если не выполнится за _ сек, бросай
         return_when=asyncio.ALL_COMPLETED
         # return_when=asyncio.FIRST_COMPLETED
     )
-    print('Асинхронно выполнено задач:    ',len(done))
-    print(done)
-    print('Асинхронно не выполнено задач: ',len(pending))
-    print(pending)
+    logger.info(f'Асинхронно выполнено задач:    {len(done)}')
+    logger.info(f'"done" tasks: {done}')
+    logger.info(f'Асинхронно не выполнено задач: {len(pending)}')
+    logger.info(f'"pending" tasks: {pending}')
     for task in pending:
-        # logger.debug("Cancelling task ", task)
+        logger.debug(f"Cancelling task {task}")
         task.cancel()
     result = None
     for task in done:
-        # logger.debug("Cancelling task ", task)
+        logger.info(f'start "done" {task}')
         if 'Message' not in task.result().keys():
             result = task.result()
             logger.info(f"Got result: {task.result().keys()}")
@@ -100,21 +131,58 @@ async def get_my_ip():
     else:
         logger.warning("Результат отсутствует")
     try:
-        print(result.keys())
+        logger.info(result.keys())
     except:
-        print(result)
+        logger.info(result)
     return result
 
-
-
-
 def run_main():
-    # asyncio.run(fetch_data(SERVICES[0]))
-    asyncio.run(get_my_ip())
+    """function for demo"""
+    asyncio.run(get_data())
 
 def run_main_():
-    # asyncio.run(fetch_data(SERVICES[0]))
-    return asyncio.run(get_my_ip())
+    """function, that returns asyncfunction's result"""
+    return asyncio.run(get_data())
 
 if __name__ == '__main__':
-    run_main()
+
+    res = run_main_()
+    logger.info(f"result's type: {type(res)}")
+    try:
+        logger.info(res.keys())
+    except:
+        pass
+
+    Base.metadata.create_all()
+    logger.info("created DB")
+    if res:
+        logger.info("begin session")
+        session = Session()
+
+        r = Rubric(text='SOME RUBRIC')
+        r = session.query(Rubric).filter_by(text="SOME RUBRIC").one()
+        session.add(r)
+        session.commit()
+
+        data = res['Items']
+        for i in range(len(data)):
+            if i % 100 == 0:
+                logger.info(f"writing item #{i}")
+            data_input = data[i]
+            data_list = ['Id', 'Caption', 'CategoryId','PublishDate',
+                         'Keywords','ContainsGeodata','IsForeign','IsArchive',
+                         'IsNew','LastUpdateDate']
+            param = [data_input[el] for el in data_list]
+            dataset_1 = MosruDataset(id_from_mosru=param[0], caption=param[1],
+                                     categoryid=param[2],publishdate=param[3],keywords=param[4],
+                                     containsgeodata=param[5], isforeign=param[6],
+                                     isarchive=param[7],isnew=param[8], lastupdatedate=param[9],
+                                     rubric=r.id
+                                     )
+            session.add(dataset_1)
+            session.commit()
+        logger.info('session closed')
+        session.close()
+
+
+
